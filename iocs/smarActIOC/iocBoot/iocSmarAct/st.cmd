070013297dd5110cde04a5dfd1875e2280830f9e
kpetersn <kmpeters@anl.gov>
2019-04-12 15:00:51 -0500

    Improved the example IOC

--- iocs/smarActIOC/iocBoot/iocSmarAct/st.cmd
+++ iocs/smarActIOC/iocBoot/iocSmarAct/st.cmd
@@ -1,8 +1,5 @@
 #!../../bin/linux-x86_64/smarAct
 
-## You may have to change smarAct to something else
-## everywhere it appears in this file
-
 < envPaths
 
 cd "${TOP}"
@@ -11,11 +8,16 @@ cd "${TOP}"
 dbLoadDatabase "dbd/smarAct.dbd"
 smarAct_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("db/xxx.db","user=kpetersn")
-
 cd "${TOP}/iocBoot/${IOC}"
+
+## motorUtil (allstop & alldone)
+dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=smarAct:")
+
+## 
+
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=kpetersn"
+## motorUtil (allstop & alldone)
+motorUtilInit("smarAct:")
+
+# Boot complete
